e24ccbeb04f2d634321ab5047719d74629065c53
Jayce Azua <jayce.azua@gmail.com>
2018-10-16 00:54:40 -0400

    fixing the testing

--- test/reviews_test.js
+++ test/reviews_test.js
@@ -1,59 +1,63 @@
-// reviews_test.js
+// test-reviews.js
+
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server');
 const should = chai.should();
 const Review = require('../models/review');
-
 chai.use(chaiHttp);
+const sampleReview =     {
+    "title": "Super Sweet Review",
+    "movieTitle": "La La Land",
+    "description": "A great review of a lovely movie.",
+    "rating": 2
+}
 
-// tell mocha you want to test Reviews (this string is taco)
+
 describe('Reviews', ()  => {
-  // make taco name for the test
-  it('Should index ALL reviews on / GET', (done) => {
-    // use chai-http to make a request to your server
+
+    // make taco name for the test
+    it('should index ALL reviews on / GET', (done) => {
+      // use chai-http to make a request to your server
+      chai.request(server)
+          // send a GET request to root route
+          .get('/')
+          // wait for response
+          .end((err, res) => {
+            // check that the response status is = 200 (success)
+            res.should.have.status(200);
+            // check that the response is a type html
+            res.should.be.html;
+            // end this test and move onto the next.
+            done();
+          });
+    });
+
+  // TEST NEW
+  it('should display new form on /reviews/new GET', (done) => {
     chai.request(server)
-        // send a GET request to root route
-        .get('/')
-        // wait for response
+      .get(`/reviews/new`)
         .end((err, res) => {
-          // check that the response status is = 200 (success)
           res.should.have.status(200);
-          // check that the response is a type html
-          res.should.be.html;
-          // end this test and move onto the next.
+          res.should.be.html
           done();
         });
   });
-
-  // TEST NEW
-  it('Should display new form on /reviews/new GET', (done) => {
+  // TEST CREATE
+  // TEST SHOW
+  it('should show a SINGLE review on /reviews/<id> GET', (done) => {
+    var review = new Review(sampleReview);
+    review.save((err, data) => {
       chai.request(server)
-        .get('/reviews/new')
+        .get(`/reviews/${data._id}`)
         .end((err, res) => {
-            res.should.have.status(200);
-            res.should.be.html
-            done();
+          res.should.have.status(200);
+          res.should.be.html
+          done();
         });
     });
-  // TEST CREATE
-  it('Should create a new review /reviews POST', (done) => {
-
-  });
-  // TEST SHOW
-  it('Should show an individual review /reviews/:id GET', (done) => {
-
   });
   // TEST EDIT
-  it('Should show the individual review to update review /reviews/:id/edit GET', (done) => {
-
-  });
   // TEST UPDATE
-  it('Should update the individual review /reviews/:id PUT', (done) => {
-
-  });
   // TEST DELETE
-  it('Sould delete individual review /reviews/:id DETELE', (done) => {
-
-  });
 });
